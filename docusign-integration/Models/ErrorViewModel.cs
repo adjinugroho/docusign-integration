6586e9953c4d0f5655ef8962ec3777674fd6ad28
Adji Nugroho <adjinugroho@hotmail.com>
2022-04-06 14:21:13 +0700

    Change project to MVC

---
+++ docusign-integration/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace docusign_integration.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
