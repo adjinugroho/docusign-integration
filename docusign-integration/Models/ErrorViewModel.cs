0cb1a19b9532746a5e11f578b295bf7476651cea
Adji Nugroho <adjinugroho@hotmail.com>
2022-04-07 05:15:44 +0700

    Remove unused file

--- docusign-integration/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace docusign_integration.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
