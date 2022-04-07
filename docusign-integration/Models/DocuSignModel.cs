4c7d10238fffa8f7d34a40b4dc15e44e71532473
Adji Nugroho <adjinugroho@hotmail.com>
2022-04-08 05:09:39 +0700

    Add DocuSign model

---
+++ docusign-integration/Models/DocuSignModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace docusign_integration.Models
+{
+    public class DocuSignModel
+    {
+        [DisplayName("Name")]
+        public string UserName { get; set; }
+
+        [DisplayName("Email")]
+        public string UserEmail { get; set; }
+
+        [DisplayName("Sign Location")]
+        public string KeyString { get; set; }
+
+        [DisplayName("File")]
+        public IFormFile? UploadFile { get; set; }
+    }
+}
