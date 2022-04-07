038e897aa049074a39c7615f969d5290ed4a4dd9
Adji Nugroho <adjinugroho@hotmail.com>
2022-04-08 05:07:22 +0700

    Add DocuSign settings

---
+++ docusign-integration/Helper/ConstantHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace docusign_integration.Helper
+{
+    public static class CfgDocuSign
+    {
+        public const string ImpersonatedUserId = "DocuSignConfig:ImpersonatedUserId";
+        public const string APIAccountId = "DocuSignConfig:APIAccountId";
+        public const string BasePath = "DocuSignConfig:BasePath";
+        public const string ClientId = "DocuSignConfig:ClientId";
+        public const string PrivateKeyFile = "DocuSignConfig:PrivateKeyFile";
+        public const string RedirectUrl = "DocuSignConfig:RedirectUrl";
+    }
+}
